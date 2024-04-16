c5640abf4ecb54dada4f28d807cd10743cfdf5f9
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-04-16 16:40:21 +0530

    Initial commit

---
+++ src/main/java/com/bookevent/BookEventManager/BookEventManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bookevent.BookEventManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEventManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookevent/BookEventManager/BookEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookevent.BookEventManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
